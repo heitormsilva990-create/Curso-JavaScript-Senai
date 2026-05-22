92e60c2b5bcaec215ff1fecff2cff4ec021dca61
Heitor <heitormsilva990@gmail.com>
2026-05-22 19:32:03 -0300

    criando funções

---
+++ js/index.js
@@ -0,0 +1,12 @@
+console.log("Executando pelo arquivo index.js")
+
+function somarNumeros (x, y){
+    console.log("Aqui é feito a soma dos Numeros")
+      let soma = x + y
+    console.log(soma)
+}
+
+somarNumeros(5, 8) // 1 chamada
+somarNumeros(10, 10) // 2 chamada
+somarNumeros(9, 5) // 3 chamada
+somarNumeros(7, 3) // 4 chamada
